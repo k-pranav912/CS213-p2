b281e80da19418824666f310f7e38021b3f60a7d
k-pranav912 <saipranav1289@gmail.com>
2021-10-07 15:13:23 -0400

    Set up some barebones classes and stuff.

--- src/studentroster/Profile.java
+++ src/studentroster/Profile.java
@@ -3,4 +3,23 @@ package studentroster;
 public class Profile {
     private String name;
     private Major major;
+
+    @Override
+    public boolean equals(Object object) {
+        if ((object == null) || (this == null)) return false;
+
+        if (!(object instanceof Profile)) return false;
+
+        Profile obj = (Profile) object;
+
+        if ((obj.name == null) || (this.name == null)) return false;
+        if ((obj.major == null) || (this.major ==  null)) return false;
+
+        return this.name.equals(obj.name) && (this.major == obj.major);
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ":" + this.major;
+    }
 }
